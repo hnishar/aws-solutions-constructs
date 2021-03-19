ba7677c08da4d2ee6e6e2b4a4d72564e3cb57c6e
Hitendra Nishar <hnishar@amazon.com>
2021-03-19 18:24:44 -0400

    Update build-cdk-dist.sh

--- deployment/build-cdk-dist.sh
+++ deployment/build-cdk-dist.sh
@@ -57,6 +57,27 @@ for dir in $(find $source_dir/patterns/\@aws-solutions-constructs/ -name dist | 
 done
 
 echo "------------------------------------------------------------------------------"
+echo "[Create] build.json file"
+echo "------------------------------------------------------------------------------"
+# Get commit from CodePipeline (or git, if we are in CodeBuild)
+# If CODEBUILD_RESOLVED_SOURCE_VERSION is not defined (i.e. local
+# build or CodePipeline build), use the HEAD commit hash).
+echo $deployment_dir
+version=$(node -p "require('$deployment_dir/get-version.js')")
+commit="${CODEBUILD_RESOLVED_SOURCE_VERSION:-}"
+if [ -z "${commit}" ]; then
+  commit="$(git rev-parse --verify HEAD)"
+fi
+
+cat > ${dist_dir}/build.json <<HERE
+{
+  "name": "aws-solutions-constructs",
+  "version": "${version}",
+  "commit": "${commit}"
+}
+HERE
+
+echo "------------------------------------------------------------------------------"
 echo "[List] deployment/dist contents"
 echo "------------------------------------------------------------------------------"
 
